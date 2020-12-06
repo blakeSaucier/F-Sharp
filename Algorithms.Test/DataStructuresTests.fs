1defcf2ef53684ed025f85746df263fae761fab8
James <jamessaucier@gmail.com>
2020-12-06 13:33:21 -0800

    add tests

---
+++ Algorithms.Test/DataStructuresTests.fs
@@ -0,0 +1,21 @@
+﻿module DataStructuresTests
+
+open Expecto
+open BST
+
+[<Tests>]
+let dataStructureTests =
+    testList "Data Structures Tests" [
+        
+        testList "Binary Search Tree Tests" [
+            test "Simple BST creation" {
+                let actual = BST.fromList [20; 10; 30]
+                let expected =
+                    Some 
+                        { Data = 20
+                          Left = Some { Data = 10; Left = None; Right = None }
+                          Right = Some { Data = 30; Left = None; Right = None }}
+                Expect.equal actual expected "Testing"
+            }
+        ]
+    ]
---
+++ Algorithms.Test/Program.fs
@@ -0,0 +1,5 @@
+﻿open Expecto
+
+[<EntryPoint>]
+let main args =
+    Tests.runTestsInAssembly defaultConfig args
