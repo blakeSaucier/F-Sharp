3bb96d7564490c45610ba462770386ea9884527f
James <jamessaucier@gmail.com>
2020-12-06 13:33:15 -0800

    Add BST module

---
+++ Algorithms/DataStructures/BST.fs
@@ -0,0 +1,17 @@
+﻿module BST
+
+type Node =
+    { Data: int
+      Left: Node option
+      Right: Node option }
+
+let rec add data tree =
+    match tree with
+    | None -> { Data = data; Left = None; Right = None }
+    | Some t ->
+        if t.Data > data then
+            { t with Left = Some (add data t.Left) }
+        else
+            { t with Right = Some (add data t.Right) }
+
+let fromList lst = lst |> List.fold (fun tree data -> Some (add data tree)) None
